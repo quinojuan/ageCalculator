ab7899c8a59b6b6642f4c890581d93d8f9e6cee9
quinojuan <quinojuan@gmail.com>
2023-12-14 09:45:49 -0300

    finished

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
 
 const calculateAge = () => {
   let birthDate = new Date(userInput.value);
-  console.log({birthDate});
+  console.log({ birthDate });
 
   let d1 = birthDate.getDate();
-  console.log({d1})
+  console.log({ d1 });
   let m1 = birthDate.getMonth() + 1;
-  console.log({m1})
+  console.log({ m1 });
   let y1 = birthDate.getFullYear();
-  console.log({y1})
+  console.log({ y1 });
 
   let today = new Date();
 
@@ -25,21 +26,21 @@ const calculateAge = () => {
   if (m2 >= m1) {
     m3 = m2 - m1;
   } else {
-    y3--;
-    m3 = 12 + m2 - m1;
+    y3--; // esto es porque no llega al año entonces le resto 1
+    m3 = 12 + m2 - m1; // total de meses menos la diferencia
   }
 
   if (d2 >= d1) {
     d3 = d2 - d1;
   } else {
     m3--;
-    d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    d3 = getDaysInMonth(y1, m1) + d2 - d1; // obtengo el total de dias del mes de nacimiento y le resto la diferencia
   }
   if (m3 < 0) {
     m3 = 11;
     y3--;
   }
-  console.log(y3, m3, d3);
+  result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months and <span>${d3}</span> days old`;
 };
 const getDaysInMonth = (year, month) => {
   return new Date(year, month, 0).getDate();
